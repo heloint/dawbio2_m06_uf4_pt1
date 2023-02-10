542e52bc24719c7b41d8b0ef2f16b277eeb52927
heloint <majerdaniel93@gmail.com>
2023-02-10 21:16:42 +0100

    to be continued with refactoring index.js

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 const path = require('path');
 const multer = require("multer");
+const requestFunctions = require("./request-functions");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -55,164 +56,17 @@ connection.connect(function (err) {
     console.log("Connected as id " + connection.threadId);
 });
 
-// MULTER FILE UPLOAD
-// ===========================================
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, 'uploads/');
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
-});
-
-const upload = multer({storage: storage });
-
-app.post('/uploadFasta', [cors(), upload.array("file")], (req, res) => {
-
-  const file = req;
-    console.log(req.body);
-    console.log(req.files);
-
-  if (!file) {
-
-    const error = new Error('Please upload a file')
-
-    error.httpStatusCode = 400
-
-    return next(error)
-
-  }
-
-    res.status(200).send({result: true})
-})
-// ===========================================
-
-
-app.get("/roles", cors(), function (req, res) {
-    console.log("Trying to get data from the database.");
-
-    connection.query(
-        `SELECT * FROM roles`,
-
-        function (error, results, field) {
-            if (error) {
-                console.log(
-                    "Error occured during the query of the bank accounts."
-                );
-                console.log("============================");
-                console.log(error);
-                console.log("============================");
-                res.status(400).send({ results: null });
-            } else {
-                res.status(200).send({ result: results });
-            }
-        }
-    );
-});
-
-app.get("/lastUserID", cors(), function (req, res) {
-    console.log("Trying to get data from the database.");
-
-    connection.query(
-        `SELECT MAX(user_id) AS last_id FROM users`,
-
-        function (error, results, field) {
-            if (error) {
-                console.log(
-                    "Error occured during the query of the bank accounts."
-                );
-                console.log("============================");
-                console.log(error);
-                console.log("============================");
-                res.status(400).send({ results: null });
-            } else {
-                res.status(200).send({ result: results[0].last_id });
-            }
-        }
-    );
-});
-
-app.get("/users", cors(), function (req, res) {
-    console.log("Trying to get data from the database.");
-
-    connection.query(
-        `SELECT
-            U.user_id,
-            U.username,
-            R.role_name,
-            U.password,
-            U.email,
-            U.first_name,
-            U.last_name,
-            U.registration_date
-         FROM users AS U JOIN roles as R ON U.role_id=R.role_id
-        `,
-
-        function (error, results, field) {
-            if (error) {
-                console.log(
-                    "Error occured during the query of the bank accounts."
-                );
-                console.log("============================");
-                console.log(error);
-                console.log("============================");
-                res.status(400).send({ results: null });
-            } else {
-                res.status(200).send({ result: results });
-            }
-        }
-    );
-});
-// ----------------------------------------------------------------
-
-// ----------------------------------------------------------------
-
-app.post("/login", cors(), function (req, res) {
-    console.log("Trying to send data from the database.");
-
-    let sql = `SELECT * FROM users WHERE username=? AND password=?`;
-
-    connection.query(
-        sql,
-        [
-            req.body.username,
-            req.body.password,
-        ],
-        function (error, result, field) {
-            if (error) {
-                console.log(
-                    "The following error has occured during querying the database:"
-                );
-                console.log("===================================");
-                console.log(error);
-                console.log("===================================");
-                res.status(400).send({ results: false});
-            } else {
-
-                console.log("Succesfully retrieved data.");
-                if (result.length > 0) {
-                    res.status(200).send({
-                        username: result[0].username,
-                        first_name: result[0].first_name,
-                        last_name: result[0].last_name,
-                        role: result[0].role,
-                    });
-                } else {
-                    res.status(200).send({});
-                }
-            }
-        }
-    );
-
-});
-// ----------------------------------------------------------------
+requestFunctions.handleFileUpload(app, cors);
+requestFunctions.handleGetRoles(app, cors, connection);
+requestFunctions.handleGetLastUserID(app, cors, connection);
+requestFunctions.handleGetUsers(app, cors, connection);
+requestFunctions.handlePostLogin(app, cors, connection);
+requestFunctions.handlePostAddUser(app, cors, connection);
+requestFunctions.handlePostUserByID(app, cors, connection);
 
 app.post("/userByID", cors(), function (req, res) {
-    console.log("Trying to send data from the database.");
-
-    let sql = `
-        SELECT
+    connection.query(
+        ` SELECT
             U.user_id,
             U.username,
             R.role_name,
@@ -222,27 +76,15 @@ app.post("/userByID", cors(), function (req, res) {
             U.last_name,
             U.registration_date
          FROM users AS U JOIN roles as R ON U.role_id=R.role_id
-         WHERE U.user_id = ?
-    `;
-
-
-    connection.query(
-        sql,
+         WHERE U.user_id = ?`,
         [
             req.body.userID,
         ],
         function (error, result, field) {
             if (error) {
-                console.log(
-                    "The following error has occured during querying the database:"
-                );
-                console.log("===================================");
                 console.log(error);
-                console.log("===================================");
                 res.status(400).send({ results: false});
             } else {
-
-                console.log("Succesfully retrieved data.");
                 if (result.length > 0) {
                     res.status(200).send({ result: result });
                 } else {
@@ -251,7 +93,6 @@ app.post("/userByID", cors(), function (req, res) {
             }
         }
     );
-
 });
 
 app.post("/deleteUserByID", cors(), function (req, res) {
@@ -332,52 +173,6 @@ app.post("/updateUser", cors(), function (req, res) {
 
 });
 
-app.post("/addUser", cors(), function (req, res) {
-    console.log("Trying to send data from the database.");
-
-    let sql = `
-        INSERT INTO users VALUES 
-        (NEXT VALUE FOR user_id, 
-            ?,
-            ( SELECT role_id
-              FROM roles
-              WHERE role_name=?
-            ),
-            ?,
-            ?,
-            ?,
-            ?,
-            now()
-        )
-    `;
-
-    connection.query(
-        sql,
-        [
-            req.body.username,
-            req.body.role_name,
-            req.body.password,
-            req.body.email,
-            req.body.first_name,
-            req.body.last_name,
-        ],
-        function (error, result, field) {
-            if (error) {
-                console.log(
-                    "The following error has occured during querying the database:"
-                );
-                console.log("===================================");
-                console.log(error);
-                console.log("===================================");
-                res.status(400).send({ results: false});
-            } else {
-              console.log("Succesfully added user.");
-              res.status(200).send({ result: true});
-            }
-        }
-    );
-
-});
 
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
--- api/request-functions.js
+++ api/request-functions.js
@@ -1,19 +1,19 @@
-const multer = require('multer');
+const multer = require("multer");
 
 /**
  * Function to configure the storage for the uploaded files
  * @function
- * @return {Object}
+ * @return {Object} - Multer configuration.
  */
 const configureStorage = () => {
-    return multer.diskStorage({
-        destination: function (req, file, callback) {
-            callback(null, 'uploads/');
-        },
-        filename: function (req, file, callback) {
-            callback(null, file.originalname);
-        }
-    });
+  return multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, "uploads/");
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    },
+  });
 };
 
 /**
@@ -21,25 +21,188 @@ const configureStorage = () => {
  * @function
  * @param {Object} app - Express application
  */
-const handleFileUpload = (app) => {
-    const storage = configureStorage();
-    const upload = multer({ storage: storage });
+const handleFileUpload = (app, cors) => {
+  const storage = configureStorage();
+  const upload = multer({ storage: storage });
 
-    app.post('/uploadFasta', [cors(), upload.array("file")], (req, res) => {
-        const file = req;
-        console.log(req.body);
-        console.log(req.files);
+  app.post("/uploadFasta", [cors(), upload.array("file")], (req, res) => {
+    const file = req;
 
-        if (!file) {
-            const error = new Error('Please upload a file');
-            error.httpStatusCode = 400;
-            return next(error);
-        }
+    if (!file) {
+      const error = new Error("Please upload a file");
+      error.httpStatusCode = 400;
+      return next(error);
+    }
 
-        res.status(200).send({ result: true });
+    res.status(200).send({ result: true });
+  });
+};
+
+/**
+ * Function to handle getting all roles.
+ * @function
+ * @param {Object} app - Express application
+ */
+const handleGetRoles = (app, cors, connection) => {
+  app.get("/roles", cors(), function (req, res) {
+    connection.query(`SELECT * FROM roles`, function (error, results, field) {
+      if (error) {
+        console.log(error);
+        res.status(400).send({ results: null });
+      } else {
+        res.status(200).send({ result: results });
+      }
     });
+  });
+};
+
+/**
+ * Function to handle getting the last occupied user ID.
+ * @function
+ * @param {Object} app - Express application
+ */
+const handleGetLastUserID = (app, cors, connection) => {
+  app.get("/lastUserID", cors(), function (req, res) {
+    connection.query(
+      `SELECT MAX(user_id) AS last_id FROM users`,
+      function (error, results, field) {
+        if (error) {
+          console.log(error);
+          res.status(400).send({ results: null });
+        } else {
+          res.status(200).send({ result: results[0].last_id });
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Function to handle getting all users from database.
+ * @function
+ * @param {Object} app - Express application
+ */
+const handleGetUsers = (app, cors, connection) => {
+  app.get("/users", cors(), function (req, res) {
+    connection.query(
+      `SELECT
+                U.user_id,
+                U.username,
+                R.role_name,
+                U.password,
+                U.email,
+                U.first_name,
+                U.last_name,
+                U.registration_date
+             FROM users AS U JOIN roles as R ON U.role_id=R.role_id
+            `,
+      function (error, results, field) {
+        if (error) {
+          console.log(error);
+          res.status(400).send({ results: null });
+        } else {
+          res.status(200).send({ result: results });
+        }
+      }
+    );
+  });
+};
+
+const handlePostLogin = (app, cors, connection) => {
+  app.post("/login", cors(), function (req, res) {
+    connection.query(
+      `SELECT * FROM users WHERE username=? AND password=?`,
+      [req.body.username, req.body.password],
+      function (error, result, field) {
+        if (error) {
+          console.log(error);
+          res.status(400).send({ results: false });
+        } else {
+          if (result.length > 0) {
+            res.status(200).send({
+              username: result[0].username,
+              first_name: result[0].first_name,
+              last_name: result[0].last_name,
+              role: result[0].role,
+            });
+          } else {
+            res.status(200).send({});
+          }
+        }
+      }
+    );
+  });
+};
+
+const handlePostAddUser = (app, cors, connection) => {
+  app.post("/addUser", cors(), function (req, res) {
+    connection.query(
+      ` INSERT INTO users VALUES
+            (NEXT VALUE FOR user_id,
+                ?,
+                ( SELECT role_id
+                  FROM roles
+                  WHERE role_name=?
+                ),
+                ?, ?, ?, ?, now()
+            ) `,
+      [
+        req.body.username,
+        req.body.role_name,
+        req.body.password,
+        req.body.email,
+        req.body.first_name,
+        req.body.last_name,
+      ],
+      function (error, result, field) {
+        if (error) {
+          console.log(error);
+          res.status(400).send({ results: false });
+        } else {
+          res.status(200).send({ result: true });
+        }
+      }
+    );
+  });
+};
+
+const handlePostUserByID = (app, cors, connection) => {
+  app.post("/userByID", cors(), function (req, res) {
+    connection.query(
+      ` SELECT
+            U.user_id,
+            U.username,
+            R.role_name,
+            U.password,
+            U.email,
+            U.first_name,
+            U.last_name,
+            U.registration_date
+         FROM users AS U JOIN roles as R ON U.role_id=R.role_id
+         WHERE U.user_id = ?`,
+      [req.body.userID],
+      function (error, result, field) {
+        if (error) {
+          console.log(error);
+          res.status(400).send({ results: false });
+        } else {
+          if (result.length > 0) {
+            res.status(200).send({ result: result });
+          } else {
+            res.status(200).send({});
+          }
+        }
+      }
+    );
+  });
 };
 
 module.exports = {
-    handleFileUpload,
+  handleFileUpload: handleFileUpload,
+  handleGetRoles: handleGetRoles,
+  handleGetLastUserID: handleGetLastUserID,
+  handleGetUsers: handleGetUsers,
+  handlePostLogin: handlePostLogin,
+  handlePostAddUser: handlePostAddUser,
+  handlePostUserByID: handlePostUserByID, 
 };
